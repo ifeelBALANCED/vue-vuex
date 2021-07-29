f9f976f2cf61f1cce9601e73c54679a92b5a638d
vlad <vladislavherasymenko@gmail.com>
2021-07-29 18:35:35 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-app>
+        <Navbar/>
+        <router-view/>
+    </v-app>
+</template>
+
+<script>
+import Navbar from "@/components/Navbar";
+
+export default {
+    name: 'App',
+    components: {
+        Navbar
+    }
+};
+</script>
